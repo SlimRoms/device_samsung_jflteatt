b622271fb5c726865515f998084d6ee6526085f3
cordell <cordellsmail@gmail.com>
2013-12-27 11:45:40 +0100

    remove ledflash
    Change-Id: Ia2d658fe4bdcbcd33fb830f77e22ba60bf0a9d3e

--- slim.mk
+++ slim.mk
@@ -12,9 +12,6 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys"
 
 PRODUCT_NAME := slim_jflteatt
